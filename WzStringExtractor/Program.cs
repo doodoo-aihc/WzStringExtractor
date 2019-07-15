dec407d19733e9872ff87650a81df65c95afb519
ikaeren <zchia0801@gmail.com>
2019-07-16 04:27:26 +0800

    Add project files.

---
+++ WzStringExtractor/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MapleLib.WzLib;
+using MapleLib.MapleCryptoLib;
+using MapleLib.Helpers;
+using MapleLib.PacketLib;
+using MapleLib.WzLib.Util;
+using MapleLib.WzLib.WzProperties;
+using reWZ;
+using reWZ.WZProperties;
+
+namespace WzStringExtractor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //WZFile xz = new WZFile(@"F:\MapleStorySEA\String.wz", WZVariant.MSEA, true, WZReadSelection.EagerParseStrings);
+            //WZStringProperty property = xz.ResolvePath("Consume.img/2000002/name").ValueOrDie<WZStringProperty>();
+            //Console.Write(property);
+            WzFile file = new WzFile($@"F:\MapleStorySEA\String.wz", 187, WzMapleVersion.GETFROMZLZ);
+            file.ParseWzFile();
+
+            file.WzDirectory.ParseImages();
+
+            foreach (var item in file.WzDirectory.WzImages)
+            {
+                item.ParseImage(true);
+                Console.WriteLine(item.Name);
+            }
+
+
+            //var test = file.GetObjectFromPath("Consume.img");
+            Console.Write("test");
+            Console.ReadKey();
+        }
+    }
+}
