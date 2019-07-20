7c8b2a61676002ace6fe0387c66b237cfa0eca56
ikaeren <zchia0801@gmail.com>
2019-07-21 05:20:25 +0800

    Add function to extract damage skin numbers

--- WzStringExtractor/ExtractDamageSkinNumbers.cs
+++ WzStringExtractor/ExtractDamageSkinNumbers.cs
@@ -33,14 +33,27 @@ namespace WzStringExtractor
                     {
                         foreach (var number in numberImg)
                         {
-                            WZCanvasProperty test = (WZCanvasProperty)number;
-                            string[] pathNames = test.Path.Split('/');
-                            int itemId = numberType["ItemID"].ValueOrDefault<Int32>(Int32.Parse(pathNames[4]));
                             if (!(number is WZCanvasProperty))
                             {
                                 break;
                             }
-                            dmgSkinNumberPng = test.Value;
+
+                            WZCanvasProperty test = (WZCanvasProperty)number;
+                            string[] pathNames = number.Path.Split('/');
+                            int itemId = numberType["ItemID"].ValueOrDefault<Int32>(Int32.Parse(pathNames[3]));
+
+
+                            if (number.HasChild("_inlink"))
+                            {
+                                string path = number["_inlink"].ValueOrDie<string>();
+                                dmgSkinNumberPng = xz.ResolvePath($"BasicEff.img/{path}").ValueOrDie<Bitmap>();
+                                
+                            }
+                            else
+                            {
+                                dmgSkinNumberPng = test.Value;
+                            }
+
                             Directory.CreateDirectory($@"{outputLocation}\{itemId.ToString()}\{pathNames[4]}");
                             dmgSkinNumberPng.Save($@"{outputLocation}\{itemId.ToString()}\{pathNames[4]}\{pathNames[5]}.png", ImageFormat.Png);
                             Console.WriteLine("Exported damage skin");
@@ -48,7 +61,38 @@ namespace WzStringExtractor
                         }
                     }
                 }
+                else
+                {
+                    foreach (var numberImg in numberType)
+                    {
+                        foreach (var number in numberImg)
+                        {
+                            if (!(number is WZCanvasProperty))
+                            {
+                                break;
+                            }
 
+                            WZCanvasProperty test = (WZCanvasProperty)number;
+                            string[] pathNames = number.Path.Split('/');
+
+                            if (number.HasChild("_inlink"))
+                            {
+                                string path = number["_inlink"].ValueOrDie<string>();
+                                dmgSkinNumberPng = xz.ResolvePath($"BasicEff.img/{path}").ValueOrDie<Bitmap>();
+
+                            }
+                            else
+                            {
+                                dmgSkinNumberPng = test.Value;
+                            }
+
+                            Directory.CreateDirectory($@"{outputLocation}\{pathNames[3]}\{pathNames[4]}");
+                            dmgSkinNumberPng.Save($@"{outputLocation}\{pathNames[3]}\{pathNames[4]}\{pathNames[5]}.png", ImageFormat.Png);
+                            Console.WriteLine($"Exported damage skin - {pathNames[3]}");
+                            count++;
+                        }
+                    }
+                }
             }
             Console.WriteLine($"Successfully dumped {count.ToString()} number of damage skins");
             Console.ReadKey();
